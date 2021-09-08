1e3db2a727055be9b6ed12f28930ef1687c9b03b
Rod Andrade <randrade@brbatel.com.br>
2021-09-07 21:39:47 -0300

    chore: add styled-comonents

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as S from './styles';
+
+
+export function Dashboard(){
+    return (
+        <S.Container>
+            <S.Title>Meu App</S.Title>
+        </S.Container>
+    )
+}
