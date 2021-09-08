764808835c1c0f5963491f027a0511659ab7508b
Rod Andrade <randrade@brbatel.com.br>
2021-09-08 00:10:30 -0300

    feat: refactor all

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -5,7 +5,17 @@ import * as S from './styles';
 export function Dashboard(){
     return (
         <S.Container>
-            <S.Title>Meu App</S.Title>
+            <S.Header>
+                <S.UserWrapper>
+                <S.UserInfo>
+                    <S.Photo source={{uri: 'https://avatars.githubusercontent.com/u/3511851?v=4'}}/>
+                    <S.User>
+                        <S.UserGreeting>Olá, </S.UserGreeting>
+                        <S.UserName>Rodrigo</S.UserName>
+                    </S.User>
+                </S.UserInfo>
+                </S.UserWrapper>
+            </S.Header>
         </S.Container>
     )
 }
