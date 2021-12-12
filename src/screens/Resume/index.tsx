1997e1108c03674af133def4f238e6a262cffa80
Rodrigo Carneiro Andrade <andrade.rodrigo@USCMAC192.local>
2021-12-12 20:53:37 -0300

    feat: finishing the filter

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -6,7 +6,11 @@ import * as S from './styles';
 import { VictoryPie } from 'victory-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useTheme } from 'styled-components';
-import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import { addMonths, subMonths, format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import locale from 'yup/lib/locale';
+import { getMonth } from 'date-fns/esm';
 
 interface TransactionData {
     type: 'positive' | 'negative'
@@ -26,16 +30,33 @@ interface CategoryData {
 }
 
 export function Resume(){
+    const [selectedDate, setSelectedDate] = useState(new Date());
     const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([]);
 
     const theme = useTheme();
+
+    function handleDateChange(action: 'next' | 'prev'){
+        if(action == 'next'){
+            const newDate = addMonths(selectedDate, 1);
+            setSelectedDate(newDate)
+        }else{
+            const newDate = subMonths(selectedDate, 1);
+            setSelectedDate(newDate)
+        }
+    }
 
     async function loadData(){
         const dataKey = '@gofinances:transactions';
         const response = await AsyncStorage.getItem(dataKey);
         const responseFormatted = response ? JSON.parse(response) : [];
 
-        const expensives = responseFormatted.filter((expensive: TransactionData) => expensive.type === 'negative')
+        const expensives = responseFormatted
+        .filter((expensive: TransactionData) => 
+            expensive.type === 'negative' &&
+            new Date(expensive.date).getMonth() === selectedDate.getMonth() &&
+            new Date(expensive.date).getFullYear() === selectedDate.getFullYear()
+
+        );
     
         const expensivesTotal = expensives.reduce((acumulator: number, expensive: TransactionData) => {
             return acumulator + Number(expensive.amount);
@@ -77,7 +98,7 @@ export function Resume(){
 
     useEffect(()=>{
         loadData();
-    },[])
+    },[selectedDate])
 
     return (
         <S.Container>
@@ -92,13 +113,13 @@ export function Resume(){
                 }}
             >
                 <S.MonthSelect>
-                    <S.MonthSelectButton>
+                    <S.MonthSelectButton onPress={() => handleDateChange('prev')}>
                         <S.MonthSelectIcon name="chevron-left" />
                     </S.MonthSelectButton>
 
-                    <S.Month>Maio</S.Month>
+                    <S.Month>{format(selectedDate,'MMMM, yyyy', {locale: ptBR})}</S.Month>
 
-                    <S.MonthSelectButton>
+                    <S.MonthSelectButton onPress={() => handleDateChange('next')}>
                         <S.MonthSelectIcon name="chevron-right" />
                     </S.MonthSelectButton>
                 </S.MonthSelect>
